71dfbe2b3c05a8d09e4b0fb516ff97c078520ef4
Alex <maydenz@gmail.com>
2017-10-25 15:38:25 +0300

    Fixing the validator

--- public/js/validator_helper.js
+++ public/js/validator_helper.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    $("#submitConvert").on('click',function(e){
+   $("#submitConvert").on('click',function(e){
         var numbersType = $("select[name='toNumber']").val();
         if(numbersType == "Arabic")
         {
             var input = $('#roman-txt').val();
-            var regex = new RegExp("(?:X(?:X(?:V(?:I(?:I?I)?)?|X(?:I(?:I?I)?)?|I(?:[VX]|I?I)?)?|V(?:I(?:I?I)?)?|I(?:[VX]|I?I)?)?|V(?:I(?:I?I)?)?|I(?:[VX]|I?I)?)");
+            var regex = new RegExp("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
             if(regex.test(input))
             {
                 $('#convertorForm').submit();
